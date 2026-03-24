1e619a6d574c5af57c98973799695d04115193d8
Anush Kundu <anushkundu55@gmail.com>
2026-03-24 17:46:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -169,9 +169,9 @@ st.markdown(
     50%{ transform: translate(30px, -20px); }
 }
 .hero-title{
-    font-size: 3.2rem;
+    font-size: 10.2rem;
     font-weight: 800;
-    color: #FFD700;
+    color: #FFFFFF;
     margin: 0 0 12px;
     letter-spacing: -0.5px;
     position: relative;
